99494f123881009fb7aa18ded4df30cae197aa82
John J Setlock II <johnsetlock@gmail.com>
2022-07-18 15:13:57 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code in this file!
+const currentUser = 'Grace Hopper';
+
+//const welcomeMessage = 'Welcome to Flatbook, ' + currentUser +'!';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+//const excitedWelcomeMessage='WELCOME TO FLATBOOK, ${currentUser}!';
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+//constshortGreeting='Welcome, ' + 
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
